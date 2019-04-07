cf2e82a76d59d386c18bcf399586fd01fa7a9037
Avinash <avinashcegian95@gmail.com>
2019-04-06 17:08:52 -0700

    Added Dynamic bar chart code

--- bar-update.js
+++ bar-update.js
@@ -4,32 +4,41 @@ https://bl.ocks.org/charlesdguthrie/11356441
 
 */
 
+const months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November","December"]
+const years = ["2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012"]
+
+
+function sleep(ms) {
+  console.log('called sleep')
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function random(max){
   return (Math.random() * (5.0-0.5) + 0.5).toFixed(2)
  }
 
- function getData(){
+// function getData(state){
 
-   var arr = [];
+//    var arr = [];
 
-   var names = Array.from({length: 10}, (e, i) => `Hotel ${i+1}`)
+//    var names = Array.from({length: 10}, (e, i) => `Hotel ${i+1}`)
 
 
-   for (var i = 0; i < 10; i++ ){
-     arr.push({name: names[i], count: random()});
-   }
+//    for (var i = 0; i < 10; i++ ){
+//      arr.push({name: names[i], count: random()});
+//    }
 
-   arr.sort(function (a, b) {
-     if (a.count > b.count) {
-       return -1;
-     }
-     if (a.count < b.count) {
-       return 1;
-     }
-     return 0;
-   });
-   return arr;	// produces array of objects {name: "", count: 0}
- }
+//    arr.sort(function (a, b) {
+//      if (a["Total Rating"] > b["Total Rating"]) {
+//        return -1;
+//      }
+//      if (a["Total Rating"] < b["Total Rating"]) {
+//        return 1;
+//      }
+//      return 0;
+//    });
+//    return arr;	// produces array of objects {name: "", count: 0}
+//  }
 
 
 
@@ -38,11 +47,10 @@ function random(max){
 
 
  // Draw the chart
- chart.draw(getData());
- setInterval(function(){
-   chart.draw(getData());
- }, 3000);	// every 3 seconds
-
+//  chart.draw(getData());
+//  setInterval(function(){
+//    chart.draw(getData());
+//  }, 3000);	// every 3 seconds
 
 
 
@@ -82,12 +90,12 @@ function random(max){
      // svg=settings.svg, x=settings.x, y=settings.y;
      //Reset domains
      this.y.domain(data.sort(function(a,b){
-       return b.count - a.count;
+       return b["Total Rating"] - a["Total Rating"];
      })
-       .map(function(d) { return d.name; }));
+       .map(function(d) { return d["Hotel Name"]; }));
 
      var barmax = d3.max(data, function(e) {
-       return e.count;
+       return e["Total Rating"];
      });
 
      this.x.domain([0,barmax]);
@@ -99,7 +107,7 @@ function random(max){
      //Bind new data to chart rows
      //Create chart row and move to below the bottom of the chart
      var chartRow = this.svg.selectAll("g.chartRow")
-       .data(data, function(d){ return d.name});
+       .data(data, function(d){ return d["Hotel Name"]});
 
      chartRow.transition()
        .duration(300)
@@ -116,9 +124,9 @@ function random(max){
        .attr("class","bar")
        .attr("x", 0)
        .attr("opacity",0)
-       .style("fill", function (d, i) { return self.color(d.name)})
+       .style("fill", function (d, i) { return self.color(d["Hotel Name"])})
        .attr("height", this.y.rangeBand())
-       .attr("width", function(d) { return self.x(d.count);})
+       .attr("width", function(d) { return self.x(d["Total Rating"]);})
 
      //Add value labels
      newRow.append("text")
@@ -128,7 +136,7 @@ function random(max){
        .attr("opacity",0)
        .attr("dy",".35em")
        .attr("dx","0.5em")
-       .text(function(d){return d.count;});
+       .text(function(d){return d["Total Rating"];});
 
      //Add Headlines
      newRow.append("text")
@@ -139,7 +147,7 @@ function random(max){
        .attr("opacity",0)
        .attr("dy",".35em")
        .attr("dx","0.5em")
-       .text(function(d){return d.name});
+       .text(function(d){return d["Hotel Name"]});
 
 
      //////////
@@ -149,7 +157,7 @@ function random(max){
      //Update bar widths
      chartRow.select(".bar").transition()
        .duration(300)
-       .attr("width", function(d) { return self.x(d.count);})
+       .attr("width", function(d) { return self.x(d["Total Rating"]);})
        .attr("height", this.y.rangeBand())
        .attr("opacity",1);
 
@@ -159,7 +167,7 @@ function random(max){
        .attr("opacity",1)
        .attr("y", this.y.rangeBand()/2)
        .tween("text", function(d) {
-         var i = d3.interpolate(+this.textContent.replace(/\,/g,''), +d.count);
+         var i = d3.interpolate(+this.textContent.replace(/\,/g,''), +d["Total Rating"]);
          return function(t) {
            this.textContent = i(t).toFixed(2);
          };
@@ -190,7 +198,93 @@ function random(max){
      chartRow.transition()
        .delay(delay)
        .duration(900)
-       .attr("transform", function(d){ return "translate(0," + self.y(d.name) + ")"; });
+       .attr("transform", function(d){ return "translate(0," + self.y(d["Hotel Name"]) + ")"; });
 
    }
  }
+
+function* getNextMonth() {
+  for(let month of months) {
+    yield month
+  }
+}
+
+function* getNextYear() {
+  for(let year of years) {
+    yield year
+  }
+}
+
+//  setInterval(function(){
+//    chart.draw(getData());
+//  }, 3000);
+
+function drawChart(state) {
+  const yearFn = getNextYear()
+  setInterval(() => {
+    const monthFn = getNextMonth()
+    const year = yearFn.next().value
+    setInterval(() => {
+      const fileName = `Data/${state}/`
+      const month = monthFn.next().value
+      const filePath = `${fileName}${year}/${month}.csv`
+      d3.csv(filePath, data => {
+        console.log(data)
+        data.sort(function (a, b) {
+          if (a["Total Rating"] > b["Total Rating"]) {
+            return -1;
+          }
+          if (a["Total Rating"] < b["Total Rating"]) {
+            return 1;
+          }
+          return 0;
+        });
+        const finalData = data.slice(0, 10)
+        console.log(finalData)
+        chart.draw(finalData)
+      })
+    }, 3000)
+  })
+
+  // await years.forEach(async year => {
+  //   await months.forEach(async month => {
+  //     d3.csv(fileName+year+'/'+month+'.csv', data => {
+  //       console.log('d3 data', year, month)
+  //       data.sort(function (a, b) {
+  //         if (a["Total Rating"] > b["Total Rating"]) {
+  //           return -1;
+  //         }
+  //         if (a["Total Rating"] < b["Total Rating"]) {
+  //           return 1;
+  //         }
+  //         return 0;
+  //       });
+  //       const finalData = data.slice(0,10)
+  //       chart.draw(finalData)
+  //     })
+  //     await sleep(300000)
+  //   })
+  // })
+
+
+    // const year = 2002
+    // const month = 'January'
+    // const filePath = `${fileName}${year}/${month}.csv`
+    // d3.csv(filePath, data => {
+    //   console.log(data)
+    //   data.sort(function (a, b) {
+    //     if (a["Total Rating"] > b["Total Rating"]) {
+    //       return -1;
+    //     }
+    //     if (a["Total Rating"] < b["Total Rating"]) {
+    //       return 1;
+    //     }
+    //     return 0;
+    //   });
+    //   const finalData = data.slice(0, 10)
+    //   console.log(finalData)
+    //   chart.draw(finalData)
+    // })
+}
+
+export default drawChart
